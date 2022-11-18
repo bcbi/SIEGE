4236d3f8d35fa018912bedb3c6e14338d11a952a
Ashlin Harris <90787010+AshlinHarris@users.noreply.github.com>
2022-11-17 21:34:37 -0500

    🔥 Remove untethered version statement

--- run_sysimage.sh
+++ run_sysimage.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -Eeu -o pipefail
 
-echo
-echo "SIEGE v0.1.0 (2022-11-15)"
-echo
-
 parent_dir="/Path/To/SIEGE"
 
 unset JULIA_LOAD_PATH
@@ -14,4 +10,3 @@ export JULIA_DEPOT_PATH=":${parent_dir}/build/depot"
 export JULIA_PKG_OFFLINE=true
 
 ${parent_dir}/julia-1.8.2/bin/julia -J${parent_dir}/build/sysimage.so "$@"
-
