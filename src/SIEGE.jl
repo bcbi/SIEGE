98c8efcf1764d066db84ca538f12dae94d54b5c1
AshlinHarris <ashlin_harris@brown.edu>
2023-06-22 15:10:33 -0400

    ⚡️ Generate precompile statements

--- src/SIEGE.jl
+++ src/SIEGE.jl
@@ -24,7 +24,7 @@ function main( path = pwd() )
 	new_environment = copy(ENV)
 	delete!(new_environment, "JULIA_LOAD_PATH");
 	new_environment["JULIA_DEPOT_PATH"] = my_depot
-	new_environment["JULIA_PROJECT"]=project_directory
+	new_environment["JULIA_PROJECT"] = project_directory
 
 	run(setenv(`$(Base.julia_cmd()) -e "import Pkg; Pkg.instantiate(); Pkg.precompile()"`, new_environment))
 
@@ -40,18 +40,31 @@ function main( path = pwd() )
 	do_not_include = ["PackageCompiler","SIEGE"]
 	package_list = filter(x -> x ∉ do_not_include, all_packages)
 
+	# Generate precompile file from package tests
+	precompile_file = tempname()
+	for package in package_list
+		write(precompile_file, "import $package\n")
+	end
+	write(precompile_file, "\n")
+	for package in package_list
+		write(precompile_file, "include(joinpath(pkgdir($package), \"test\", \"runtests.jl\"))\n")
+	end
+
 	# Create sysimage 
 	create_sysimage(package_list;
-		sysimage_path=my_sysimage,
-		project=project_directory,
-		incremental=true,
-		filter_stdlibs=false,
-		include_transitive_dependencies=true,
+		sysimage_path = my_sysimage,
+		project = project_directory,
+		incremental = true,
+		filter_stdlibs = false,
+		include_transitive_dependencies = true,
+		precompile_execution_file = precompile_file,
 	)
 
-	# Delete compiled and logs from local depot
-	rm(joinpath(my_depot, "compiled"), recursive=true, force=true)
-	rm(joinpath(my_depot, "logs"), recursive=true, force=true)
+	# Delete local files from build depot
+	rm(joinpath(my_depot, "compiled"), recursive = true, force = true)
+	rm(joinpath(my_depot, "logs"), recursive = true, force = true)
+	rm(joinpath(my_depot, "registries", "General.tar.gz"), force = true)
+	rm(joinpath(my_depot, "registries", "General.toml"), force = true)
 
 	return
 end
